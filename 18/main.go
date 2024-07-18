e46b1f73a84c2cf77d358a799bf79ab1f285d3d5
jhonathannc <jhonathannc@gmail.com>
2024-07-17 23:12:58 -0300

    class 18

--- 18/main.go
+++ 18/main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 
 	"pos-go/matematica"
+
+	"github.com/google/uuid"
 )
 
 func main() {
 	res := matematica.Soma(10, 20)
 	fmt.Printf("Resultado: %v", res)
+
+	fmt.Println(uuid.New())
 }
