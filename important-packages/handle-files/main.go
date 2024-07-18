4db75f7959d9f4f2fe1ede56b1227dd647fa7324
jhonathannc <jhonathannc@gmail.com>
2024-07-17 23:57:39 -0300

    class handle file with chunk

---
+++ important-packages/handle-files/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Create("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	// tamanho, err := f.WriteString("hello world")
+	tamanho, err := f.Write([]byte("Escrevendo dados no arquivo"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Arquivo criado com sucesso. Tamanho: %d bytes\n", tamanho)
+	f.Close()
+
+	// reading
+	arquivo, err := os.ReadFile("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(arquivo))
+
+	// reading opening file with chunk
+	arquivo2, err := os.Open("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+	reader := bufio.NewReader(arquivo2)
+	buffer := make([]byte, 10)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(buffer[:n]))
+	}
+
+	err = os.Remove("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+}
