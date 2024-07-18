58ac5b84e3e87032519998844fc7998524e44572
jhonathannc <jhonathannc@gmail.com>
2024-07-17 23:19:50 -0300

    class 19: loopings

---
+++ 19/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	// for i := 0; i < 10; i++ {
+	// 	println(i)
+	// }
+
+	// numeros := []string{"um", "dois", "tres"}
+	// for _, value := range numeros {
+	// 	println(value)
+	// }
+
+	// while like
+	// i := 0
+	// for i < 10 {
+	// 	println(i)
+	// 	i++
+	// }
+
+	// loop infinito
+	for {
+		println("Hello World!")
+	}
+}
