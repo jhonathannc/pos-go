c76e047d592811d29e8ff1d388f4444bcb4fbf75
jhonathannc <jhonathannc@gmail.com>
2024-08-30 23:59:33 -0300

    class lock pessimista lock otimista

---
+++ banco-dados/7/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type Category struct {
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product
+}
+
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryID int
+	Category   Category
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&Product{}, &Category{})
+
+	tx := db.Begin()
+	var c Category
+	err = tx.Debug().Clauses(clause.Locking{Strength: "UPDATE"}).First(&c, 8).Error
+	if err != nil {
+		panic(err)
+	}
+	c.Name = "Eletronicos"
+	tx.Debug().Save(&c)
+	tx.Commit()
+}
