8590cb6f3497cd4b3b6d65f170720fb8a695abbe
jhonathannc <jhonathannc@gmail.com>
2024-09-02 22:09:27 -0300

    class acessing created modules

--- packaging/1/cmd/main.go
+++ packaging/1/cmd/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/jhonathannc/packaging/1/math"
+)
+
 func main() {
-	println("Hello, World!")
+	m := math.Math{A: 10, B: 20}
+	fmt.Println(m.Sum())
 }
