f22bf9a5f524060e8788c0896544c846569e3306
jhonathannc <jhonathannc@gmail.com>
2024-09-08 20:51:21 -0300

    apis: using chi library for routing

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jhonathannc/pos-go/apis/configs"
 	"github.com/jhonathannc/pos-go/apis/internal/entity"
 	"github.com/jhonathannc/pos-go/apis/internal/infra/database"
@@ -24,6 +26,11 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+
+	http.ListenAndServe(":8000", r)
 }
