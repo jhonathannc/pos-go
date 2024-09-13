77fa421e3584be2cf0ce348b64f21fc9fcc8dfaf
jhonathannc <jhonathannc@gmail.com>
2024-09-12 23:51:16 -0300

    apis: docs for user route

--- apis/internal/infra/webserver/handlers/user_handlers.go
+++ apis/internal/infra/webserver/handlers/user_handlers.go
@@ -11,6 +11,10 @@ import (
 	"github.com/jhonathannc/pos-go/apis/internal/infra/database"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 type UserHandler struct {
 	UserDB       database.UserInterface
 	Jwt          *jwtauth.JWTAuth
@@ -54,6 +58,16 @@ func (h *UserHandler) GetJwt(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(accessToken)
 }
 
+// Create user godoc
+// @Summary      Create user
+// @Description  Create user
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        request     body      dto.CreateUserInput  true  "user request"
+// @Success      201
+// @Failure      500         {object}  Error
+// @Router       /users [post]
 func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
