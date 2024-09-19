594b01e3b54cd9cdbf43cc3ec181b5b3cdc3af75
jhonathannc <jhonathannc@gmail.com>
2024-09-18 23:14:15 -0300

    go routines

---
+++ multithreading/01-go-routines/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d: Task %s is running\n", i, name)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// Thread 1
+func main() {
+	go task("A")
+	go task("B")
+
+	time.Sleep(15 * time.Second)
+}
