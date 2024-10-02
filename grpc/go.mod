ec25165c163f62f7ff9568ae867e213ebd57a748
jhonathannc <jhonathannc@gmail.com>
2024-10-01 23:35:55 -0300

    grpc: project setup

---
+++ grpc/go.mod
@@ -0,0 +1,5 @@
+module github.com/jhonathannc/grpc
+
+go 1.21.5
+
+require github.com/google/uuid v1.6.0
---
+++ grpc/internal/database/category.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type Category struct {
+	db          *sql.DB
+	ID          string
+	Name        string
+	Description string
+}
+
+func NewCategory(db *sql.DB) *Category {
+	return &Category{db: db}
+}
+
+func (c *Category) Create(name, description string) (Category, error) {
+	id := uuid.New().String()
+
+	_, err := c.db.Exec("insert into categories(id, name, description) values (?, ?, ?)", id, name, description)
+	if err != nil {
+		return Category{}, err
+	}
+
+	return Category{ID: id, Name: name, Description: description}, nil
+}
+
+func (c *Category) FindAll() ([]Category, error) {
+	rows, err := c.db.Query("select id, name, description from categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	categories := []Category{}
+	for rows.Next() {
+		var id, name, description string
+		if err := rows.Scan(&id, &name, &description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, Category{ID: id, Name: name, Description: description})
+	}
+	return categories, nil
+}
+
+func (c *Category) FindByCourseID(courseID string) (Category, error) {
+	var id, name, description string
+	err := c.db.QueryRow("select c.id, c.name, c.description from categories c join courses co on c.id = co.category_id where co.id = ?", courseID).Scan(&id, &name, &description)
+	if err != nil {
+		return Category{}, err
+	}
+	return Category{ID: id, Name: name, Description: description}, nil
+}
