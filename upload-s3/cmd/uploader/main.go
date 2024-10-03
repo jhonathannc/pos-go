3a10de20e0ab3866928f973248cd8aa65dfa410f
jhonathannc <jhonathannc@gmail.com>
2024-10-02 23:18:59 -0300

    uploads3: upload function

--- upload-s3/cmd/uploader/main.go
+++ upload-s3/cmd/uploader/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -28,4 +31,34 @@ func init() {
 	}
 	s3Client = s3.New(sess)
 	s3Bucket = "goexpert-bucket-example"
+}
+
+func main() {
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+
+}
+
+func uploadFile(filename string) {
+	completeFileName := fmt.Sprintf("./tmp/%s", filename)
+	fmt.Printf("Uploading file: %s\n", filename)
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer f.Close()
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(filename),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("Error uploading file: %v\n", err)
+		return
+	}
+	fmt.Printf("File uploaded: %s\n", filename)
 }
