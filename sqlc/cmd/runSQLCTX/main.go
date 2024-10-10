fde9c812d1cfb97cac559568047b0a21dce90fcc
jhonathannc <jhonathannc@gmail.com>
2024-10-09 22:57:44 -0300

    sqlc: working with transactions

---
+++ sqlc/cmd/runSQLCTX/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/jhonathannc/pos-go/sqlc/internal/db"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type CourseDB struct {
+	dbConn *sql.DB
+	*db.Queries
+}
+
+func NewCourseDB(dbConn *sql.DB) *CourseDB {
+	return &CourseDB{
+		dbConn:  dbConn,
+		Queries: db.New(dbConn),
+	}
+}
+
+type CourseParams struct {
+	ID          string
+	Name        string
+	Description sql.NullString
+}
+
+type CategoryParams struct {
+	ID          string
+	Name        string
+	Description sql.NullString
+}
+
+func (c *CourseDB) callTx(ctx context.Context, fn func(*db.Queries) error) error {
+	tx, err := c.dbConn.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	queries := db.New(tx)
+	err = fn(queries)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+func (c *CourseDB) CreateCourseAndCategory(ctx context.Context, argsCategory CategoryParams, argsCourse CourseParams) error {
+	err := c.callTx(ctx, func(q *db.Queries) error {
+		var err error
+		err = q.CreateCategory(ctx, db.CreateCategoryParams{
+			ID:          argsCategory.ID,
+			Name:        argsCategory.Name,
+			Description: argsCategory.Description,
+		})
+		if err != nil {
+			return err
+		}
+		err = q.CreateCourse(ctx, db.CreateCourseParams{
+			ID:          argsCourse.ID,
+			Name:        argsCourse.Name,
+			Description: argsCourse.Description,
+			CategoryID:  argsCategory.ID,
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	dbConn, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
+
+	// queries := db.New(dbConn)
+
+	courseArgs := CourseParams{
+		ID:          "1",
+		Name:        "Go Expert",
+		Description: sql.NullString{String: "Go Expert description", Valid: true},
+	}
+	categoryArgs := CategoryParams{
+		ID:          "1",
+		Name:        "Backend",
+		Description: sql.NullString{String: "Backend description", Valid: true},
+	}
+	courseDB := NewCourseDB(dbConn)
+	err = courseDB.CreateCourseAndCategory(ctx, categoryArgs, courseArgs)
+	if err != nil {
+		panic(err)
+	}
+}
