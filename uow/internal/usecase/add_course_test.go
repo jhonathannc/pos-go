19cc0c1aec833afcf828fad831273e8b60f97bb6
jhonathannc <jhonathannc@gmail.com>
2024-10-15 23:22:26 -0300

    uow: interface for uow

--- uow/internal/usecase/add_course_test.go
+++ uow/internal/usecase/add_course_test.go
@@ -1,1 +1,34 @@
 package usecase
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+
+	"github.com/jhonathannc/pos-go/uow/internal/repository"
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestAddCourse(t *testing.T) {
+	dbt, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	assert.NoError(t, err)
+
+	dbt.Exec("DROP TABLE IF EXISTS `courses`;")
+	dbt.Exec("DROP TABLE IF EXISTS `categories`;")
+
+	dbt.Exec("CREATE TABLE categories (id int PRIMARY KEY AUTO_INCREMENT, name varchar(255) NOT NULL, description varchar(255));")
+	dbt.Exec("CREATE TABLE courses (id int PRIMARY KEY AUTO_INCREMENT, name varchar(255) NOT NULL, category_id int NOT NULL);")
+
+	input := InputUseCase{
+		CategoryName:     "Category 1",
+		CourseName:       "Course 1",
+		CourseCategoryID: 1,
+	}
+
+	ctx := context.Background()
+	useCase := NewAddCourseUseCase(repository.NewCourseRepository(dbt), repository.NewCategoryRepository(dbt))
+	err = useCase.Execute(ctx, input)
+	assert.NoError(t, err)
+}
